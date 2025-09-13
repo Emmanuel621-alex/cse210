39b2a88f80382ea81c069ae031b9439aaede8b96
Emmanuel621-alex <alexanderedache4@gmauil.com>
2025-09-13 22:45:15 +0100

    journalAPP

--- obj/Debug/net9.0/cse210.AssemblyInfo.cs
+++ obj/Debug/net9.0/cse210.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cse210")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387af434ebd52176d3641fe08599853b3337c089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31734910642558840c3b248a5a8b8101ac19298")]
 [assembly: System.Reflection.AssemblyProductAttribute("cse210")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cse210")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
